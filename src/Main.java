34c46441a682321331a462a44acb9410aa1320e4
rosierludivine <ludivine.rosier38@gmail.com>
2023-07-11 09:14:40 +0200

    Exo 1 reprise

--- src/Main.java
+++ src/Main.java
@@ -2,34 +2,21 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        Dalton temel = Dalton.JACK;
-        System.out.println(temel.toString());
-        System.out.println(temel);
-        Dalton data = Dalton.valueOf("Jack");
-
 
     }
-}
- enum Dalton
-{
-    JOE (1.48, 52),
-    WILLIAM (1.68, 72),
-    JACK (1.93, 83),
-    AVERELL (2.13, 89);
+    String arrayEmail[] =
+            {
+                    "jpp@sfr.fr",
+                    "tom@gmail.com",
+                    "fred@sfr.fr",
+                    "victor@sfr.fr",
+                    "chris@sfr.fr",
+                    "robert@orange.fr",
+                    "paul@sfr.fr",
+                    "lise@gmail.com",
+                    "thierry@isitech.fr",
+                    "marie@isitech.fr"
+            };
+    String val = new String(arrayEmail);
 
-
-    private final double taille;
-    private final double poids;
-    private Dalton(double taille, double poids)
-
-    {
-        this.taille taille;
-        this.poids poids;
-    }
-    public double taille() { return taille; }
-    public double poids() { return poids; }
-    double inc()
-    {
-        return poids/(taille taille);
-    }
 }
