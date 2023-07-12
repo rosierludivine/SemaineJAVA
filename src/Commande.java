5393ba297fde7b8e52ec06cac22135cd921094ed
rosierludivine <ludivine.rosier38@gmail.com>
2023-07-12 13:52:17 +0200

    debut exercice 7

---
+++ src/Commande.java
@@ -0,0 +1,11 @@
+/*
+Créer une classe représentant une commande d’un magasin de vente par en ligne.
+Une commande est caractérisée par
+son numéro,sa date,le client qui passe la commande,
+le tableau des articles commandés
+et le tableau des quantités commandées pour chaque article.
+Créer ensuite une méthode main permettant de tester le bon fonctionnement de la classe précédente.
+*/
+public class Commande {
+
+}
